c295f623ad5ab22d7137c804e8f6713240e6ef2d
aec7yb <60191740+aec7yb@users.noreply.github.com>
2021-04-21 18:06:49 -0500

    Suggested fix
    I added a commented out line of code at line #24. The text area used above it wasn't appearing correctly on my page.
    If you can reproduce the issue, try using the commented out line as a potential workaround.

--- new_advertisement.php
+++ new_advertisement.php
@@ -21,6 +21,7 @@
                     <tr><td>Ad Title: </td><td><input type="varchar(30)" name="AdvTitle"></td><tr>
                     <tr><td>Price: </td><td><input type="varchar(30)" name="AdvPrice"></td><tr>
                     <tr><td>Details: </td><td><textarea type="varchar(50)" name="AdvDetails"></td><tr>
+		    <!-- <tr><td>Details:</td><td><textarea rows="5" cols="50" name="AdvDetails"></textarea></td><tr> -->	
                     <tr><td>Category: </td>
                         <td>
                             <input type="radio" name="Category_ID" value="CAT">Cars and Trucks

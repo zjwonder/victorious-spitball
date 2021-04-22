b81cfa8073db83a12b27d719c70d19b0f91edb1d
aec7yb <60191740+aec7yb@users.noreply.github.com>
2021-04-21 21:06:55 -0500

    Updated login.php
    Added back in function to compare password entered by user to the encrypted value set in the USERS table.
    This is based off of sample code shared by the professor and is dependent on using encrypted strings for passwords.

--- login.php
+++ login.php
@@ -1,10 +1,11 @@
-<!-- login code from: https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php -->
+<!-- login code from: https://www.tutorialrepublic.com/php-tutorial/php-mysql-login-system.php 
+	 hash values from: https://www.onlinewebtoolkit.com/hash-generator -->
 
 <?php
  
-	// Check if the user is already logged in. If yes, then redirect him to welcome page
+	// Check if the user is already logged in. If yes, then redirect them to new_advertisement.php
 	if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-		header("location: home.html"); // MIGHT NEED TO CHANGE TO new_advertisement.php
+		header("location: home.html");
 		exit;
 	}
 	 
@@ -21,11 +22,11 @@
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	 
 		// Check if login is empty
-		if (empty(trim($_POST["login"]))) { // THIS VALUE MUST BE SET TO login 
+		if (empty(trim($_POST["login"]))) {
 			$login_err = "Please enter login.";
 		} 
-		else {
-			$login = trim($_POST["login"]); // THIS VALUE MUST BE SET TO login 
+		else { 
+			$login = trim($_POST["login"]);
 		}
 		
 		// Check if password is empty
@@ -38,11 +39,10 @@
 		
 		// Validate credentials
 		if (empty($login_err) && empty($password_err)) {
-			
-			// Prepare a select statement
+			// Prepare a select statement to search from USERS table
 			$sql = "SELECT User_ID, password FROM users WHERE User_ID = ?";
 			
-			if($stmt = mysqli_prepare($connection, $sql)) {
+			if ($stmt = mysqli_prepare($connection, $sql)) {
 				// Bind variables to the prepared statement as parameters
 				mysqli_stmt_bind_param($stmt, "s", $param_login);
 				
@@ -55,40 +55,38 @@
 					mysqli_stmt_store_result($stmt);
 					
 					// Check if login exists, if yes then verify password
-					if (mysqli_stmt_num_rows($stmt) == 1) {   
-					
+					if (mysqli_stmt_num_rows($stmt) == 1) {                    
 						// Bind result variables
-						mysqli_stmt_bind_result($stmt, $login);
-						
+						mysqli_stmt_bind_result($stmt, $login, $hashed_password);
 						if (mysqli_stmt_fetch($stmt)) {
-							// Password is correct, so start a new session
-							session_start();
-							
-							// Store data in session variables
-							$_SESSION["loggedin"] = true;
-							$_SESSION["login"] = $login;                            
-							
-							// Redirect user to new_advertisement.php
-							//header("location: home.html");
-							header("location: new_advertisement.php");
-							
-						} 
-						else {
-							// Display an error message if password is not valid
-							$password_err = "The password you entered was not valid.";
+							if (password_verify($password, $hashed_password)) {
+								// Password is correct, so start a new session
+								session_start();
+								
+								// Store data in session variables
+								$_SESSION["loggedin"] = true;
+								$_SESSION["login"] = $login;                            
+								
+								// Redirect user to welcome page
+								header("location: new_advertisement.php");
+							} 
+							else {
+								// Display an error message if password is not valid
+								$password_err = "The password you entered was not valid.";
+							}
 						}
-					}
+					} 
 					else {
 						// Display an error message if login doesn't exist
 						$login_err = "No account found with that login.";
 					}
+				} 
+				else {
+					echo "Oops! Something went wrong. Please try again later.";
 				}
-			} 
-			else {
-				echo "Oops! Something went wrong. Please try again later.";
+				// Close statement
+				mysqli_stmt_close($stmt);
 			}
-			// Close statement
-			mysqli_stmt_close($stmt);
 		}
 		// Close connection
 		mysqli_close($connection);
@@ -122,7 +120,7 @@
                 <span class="help-block"><?php echo $password_err; ?></span>
             </div>
             <div class="form-group">
-                <input type="submit" class="btn btn-primary" value="Login">
+                <input type="submit" class="btn btn-primary" value="Loginxxx">
             </div>
         </form>
     </div>    

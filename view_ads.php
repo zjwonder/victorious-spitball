4f1be213a05740e40da6bb4a1b129f6ee68decd6
aec7yb <60191740+aec7yb@users.noreply.github.com>
2021-04-24 16:47:37 -0500

    New View Ads page
    Created a page that displays all rows from the 'advertisements' table.
    The only issue I think we need to address is ensuring the user stays logged in.

---
+++ view_ads.php
@@ -0,0 +1,54 @@
+<html>
+	<head>
+		<title>View Ads</title>
+		<link rel="stylesheet" type="text/css" href="home.css">
+	</head>
+	<body>
+		<div class="navigationBar">
+			<a href="home.html">Home</a>
+			<a href="view_ads.php">View Ads</a>
+			<a href="login.php">Login</a>
+			<a href="register.php">Register</a>
+		</div>	
+		<div style = "padding-left: 16px">
+		<?php
+			require_once 'connection.php';
+
+			// Querying the table
+			$sql_of_q1 = "SELECT * FROM Advertisements";
+			$q1result = mysqli_query($connection, $sql_of_q1);
+		?>
+		<div style="margin-right: 100px">
+		<table border="1">
+			<?php
+				echo "<tr>";
+				echo "<td>".'Advertisement_ID'."</td>";
+				echo "<td>".'AdvTitle'."</td>";
+				echo "<td>".'AdvDetails'."</td>";
+				echo "<td>".'AdvDate'."</td>";
+				echo "<td>".'AdvPrice'."</td>";
+				echo "<td>".'Category_ID'."</td>";
+				echo "<td>".'User_ID'."</td>";
+				echo "<td>".'Moderator_ID'."</td>";
+				echo "<td>".'Status_ID'."</td>";
+				echo "</tr>";
+		
+				while($r = mysqli_fetch_assoc($q1result)) {
+					//fetches a result row as an associative array.
+					echo "<tr>";
+					echo "<td>".$r['Advertisement_ID']."</td>";
+					echo "<td>".$r['AdvTitle']."</td>";
+					echo "<td>".$r['AdvDetails']."</td>";
+					echo "<td>".$r['AdvDate']."</td>";
+					echo "<td>".$r['AdvPrice']."</td>";
+					echo "<td>".$r['Category_ID']."</td>";
+					echo "<td>".$r['User_ID']."</td>";
+					echo "<td>".$r['Moderator_ID']."</td>";
+					echo "<td>".$r['Status_ID']."</td>";
+					echo "</tr>";
+				}
+			?>
+		</table>
+		</div>
+	</body>
+</html>

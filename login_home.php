6c57bb20cd623bd7570189d0f3b1196caa030c32
zhalderwood <zju.0228@gmail.com>
2021-04-27 14:25:05 -0500

    Update CSS formatting

--- login_home.php
+++ login_home.php
@@ -17,19 +17,31 @@
 	<head>
 		<title>Ad Manager Home</title>
 		<link rel="stylesheet" type="text/css" href="home.css">
-	</head>	
+	</head>
 	<body>
 		<div class="navigationBar">
 			<a href="login_home.php">Home</a>
 			<a href="view_ads.php">View Public Ads</a>
 			<a href="new_advertisement.php">Create Ad</a>
 			<a href="logout.php">Logout</a>
-		</div>	
+		</div>
+
+    <img src="https://thumbs.gfycat.com/OffensiveOpulentGecko.webp" alt="Computer" width="200"
+           height="100">
+
+    <center>
 		<div style = "padding-left: 16px">
-			<h2>Welcome to the Advertisements Manager!</h2>
+			<h2>Welcome to <br> the <br> <u>Advertisements Manager</u>!</h2>
+      <img src="https://thumbs.gfycat.com/AcidicHardtofindEmeraldtreeskink-max-1mb.gif" alt = "Man" width="150"
+      height="150">
+
 			<h4>Use the navigation bar above to access various pages.</h4>
-			<h4><u>Home</u> will take you... Home!</h4>
-			<h4><u>Create Ad</u> will take you to a login page so that you can create an ad!</h4>
-			<h4><u>View Ads</u> will take you to a page listing all ads! !WORK IN PROGRESS!</h4>
+			<h4><a href="login_home.php"><b>Home</b></a> will take you... Home!</h4>
+			<h4><a href="new_advertisement.php"><b>Create Ad</b></a> will take you to a login page so that you can create an ad!</h4>
+			<h4><a href="view_ads.php"><b>View Ads</b></a> will take you to a page listing all ads!</h4>
+    </center>
+
+
+
 	</body>
 </html>
--- success.php
+++ success.php
@@ -18,14 +18,29 @@ exit;
 		<title>Advertisements Manager Database</title>
 		<link rel="stylesheet" type="text/css" href="home.css">
 	</head>
-	
+
 	<body>
-		
-		<div style = "padding-left: 16px">
+
+		<div class="navigationBar">
+			<a href="home.html">Home</a>
+			<a href="view_ads.php">View Ads</a>
+			<a href="login.php">Login</a>
+			<a href="register.php">Register</a>
+		</div>
+
+		<img src="https://thumbs.gfycat.com/OffensiveOpulentGecko.webp" alt="Computer" width="200"
+           height="100">
+    <center>
+	  <div class="centerThings">
 			<h2>Success!</h2>
             <h3>Your ad has been submitted and is pending mod approval.</h3>
-			<h4>Redirecting you momentarily...</h4>
-			
+			<h4>Redirecting you momentarily or <a href="home.html">click here</a> to go home </h4>
+		</div>
+    </center>
+      <center>
+			<img src="https://images.unsplash.com/photo-1534180003648-8f8d6c814cdb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8N3x8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60" alt="umbrella" width="500"
+	           height="400">
+      </center>
 	</body>
-	
+
 </html>
